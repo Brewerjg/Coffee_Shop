91a9319b5904b138dd96353c12aa96b5ef7fac11
Jason <grammerjasonl@gmail.com>
2023-11-14 14:03:36 -0600

    reverted

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 import AnimatedRoutes from './components/AnimatedRoutes';
 
@@ -8,16 +8,7 @@ function App() {
   return (
     <div>
     	<BrowserRouter>
-        <Routes location={location} key={location.pathname}>
-            <Route element={<Main/>} path="/" default/>  
-            <Route element={<Menu/>} path="/menu" />
-            <Route element={<TruckMap/>} path="/location" />
-            <Route element={<About/>} path="/about" />
-            <Route element={<Login/>} path="/login" />
-            <Route element={<Dash/>} path="/dashboard" />
-            <Route element={<Register/>} path="/register" />
-            <Route element={<Update/>} path="/employee/:id"/>
-        </Routes>
+            <AnimatedRoutes/>
     	</BrowserRouter>
         </div>
   );
