2862a839b1665b6a81fa6555c87fd48ed0d70db8
Brewerjg <grammerjasonl@gmail.com>
2023-12-10 12:50:44 -0600

    changed cors

--- server/server.js
+++ server/server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
+
 
 require('./config/mongoose.config'); 
-// require('dotenv').config();
-app.use(cookieParser());
-// Change the app.use(cors()) to the one below
-app.use(cors({origin: 'http://localhost:3000'}));
+
+app.use(cors())
 app.use(express.json({limit: '30mb'}));                          
 app.use(express.urlencoded({limit: '30mb', extended: true }));   
 
